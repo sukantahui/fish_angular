a29ff822f412057be08579d314800b301ca6b610
Sreeparna Das <sreeparnadas0675@gmail.com>
2020-08-19 23:00:29 +0530

    save vendor

--- src/app/pages/vendor/vendor.component.ts
+++ src/app/pages/vendor/vendor.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {VendorService} from '../../services/vendor.service';
 import {Vendor} from '../../models/vendor.model';
-import {FormControl} from '@angular/forms';
-
+import {FormControl, FormGroup} from '@angular/forms';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
@@ -16,7 +16,7 @@ export class VendorComponent implements OnInit {
   pageSize = 15;
   filter = new FormControl('');
   p = 1;
-
+  vendorForm: FormGroup;
   constructor(private vendorService: VendorService) { }
 
   ngOnInit(): void {
@@ -25,6 +25,35 @@ export class VendorComponent implements OnInit {
       this.vendorList = response;
       console.log('After getting data from service ', this.vendorList);
     });
+    this.vendorForm = this.vendorService.vendorForm;
   }
 
+  clearVendorForm() {
+    this.vendorForm.reset();
+  }
+
+  onSubmit() {
+    this.vendorService.saveVendor(this.vendorForm.value).subscribe(response => {
+      if (response.success === 1){
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Product has been saved',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      }
+      }, (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error,
+          footer: '<a href>Why do I have this issue?</a>',
+          timer: 0
+        });
+      });
+  }
+  updateVendor() {
+
+  }
 }
--- src/app/services/vendor.service.ts
+++ src/app/services/vendor.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Vendor} from '../models/vendor.model';
-import {HttpClient} from '@angular/common/http';
-import {Subject} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Subject, throwError} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {catchError, tap} from "rxjs/operators";
 
 
 @Injectable({
@@ -11,6 +13,7 @@ import {Subject} from 'rxjs';
 export class VendorService {
   vendorList: Vendor[] = [];
   vendorSubject = new Subject<Vendor[]>();
+  vendorForm: FormGroup;
   constructor(private http: HttpClient) {
     this.http.get('http://127.0.0.1:8000/api/vendors')
       .subscribe((response: {success: number, data: Vendor[]}) => {
@@ -18,6 +21,24 @@ export class VendorService {
         this.vendorList = data;
         this.vendorSubject.next([...this.vendorList]);
       });
+
+    this.vendorForm = new FormGroup({
+      id : new FormControl(null),
+      person_name : new FormControl(null, [Validators.required, Validators.maxLength(20), Validators.minLength(4)]),
+      email : new FormControl(null, [Validators.required, Validators.email]),
+      mobile1 : new FormControl('+91', [Validators.maxLength(10)]),
+      mobile2 : new FormControl('+91', [Validators.maxLength(10)]),
+      person_type_id : new FormControl(11),
+      customer_category_id : new FormControl(1),
+      address1 : new FormControl(null),
+      address2 : new FormControl(null),
+      state : new FormControl('West Bengal'),
+      po : new FormControl(null),
+      area : new FormControl(null),
+      city : new FormControl(null),
+      pin : new FormControl(null, [Validators.pattern('^[0-9]*$'), Validators.maxLength(6)])
+    });
+
   } // end of constructor
   getVendorUpdateListener(){
     return this.vendorSubject.asObservable();
@@ -25,5 +46,21 @@ export class VendorService {
 
   getVendorList(){
     return [...this.vendorList];
+  }
+
+  saveVendor(vendor) {
+    return this.http.post<{ success: number, data: object }>('http://127.0.0.1:8000/api/vendors', vendor)
+      .pipe(catchError(this.handleError), tap((response: {success: number, data: Vendor}) => {
+        this.vendorList.unshift(response.data);
+        this.vendorSubject.next([...this.vendorList]);
+      }));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse){
+    if (errorResponse.error.message.includes('1062')){
+      return throwError('Record already exists');
+    }else {
+      return throwError(errorResponse.error.message);
+    }
   }
 }
