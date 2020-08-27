f3d0d92c0cf58fdffb094c6be7834415aa21caec
sukanta hui <huisukanta77@gmail.com>
2020-08-27 15:08:19 +0530

    productList added to purchase

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -4,6 +4,8 @@ import {FormControl, FormGroup} from '@angular/forms';
 import {VendorService} from '../../services/vendor.service';
 import {Vendor} from '../../models/vendor.model';
 import {ProductCategory} from "../../models/productCategory.model";
+import {Product} from '../../models/product.model';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-purchase',
@@ -18,13 +20,18 @@ export class PurchaseComponent implements OnInit {
   transactionDetail: FormGroup;
   vendorList: Vendor[] = [];
   productCategoryList: ProductCategory[] = [];
-  constructor(private purchaseService: PurchaseService, private vendorService: VendorService) { }
+  productList: Product[] = [];
+  constructor(private purchaseService: PurchaseService, private vendorService: VendorService, private prodcutService: ProductService) { }
 
   ngOnInit(): void {
     this.productCategoryList = this.purchaseService.getProductCategoryList();
     this.purchaseService.getProductCategoryUpdateListener().subscribe(response => {
         // console.log(response);
         this.productCategoryList = response;
+    });
+    this.productList = this.prodcutService.getProducts();
+    this.prodcutService.getProductUpdateListener().subscribe(response => {
+      this.productList = response;
     });
     this.purchaseMasterForm = this.purchaseService.purchaseMasterForm;
     this.purchaseDetailForm = this.purchaseService.purchaseDetailForm;
