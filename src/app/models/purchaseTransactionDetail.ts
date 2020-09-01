092c8d55c3d531b9056d6062d39fe4485c0f18bd
sukanta hui <huisukanta77@gmail.com>
2020-09-01 21:10:15 +0530

    working on purchase bill

--- src/app/models/purchaseTransactionDetail.ts
+++ src/app/models/purchaseTransactionDetail.ts
@@ -1,15 +1,4 @@
 
-// export class PurchaseTransactionDetail{
-//   id?: number;
-//   transaction_date: string;
-//   transaction_number: string;
-//   voucher_id: number;
-//   purchase_master_id: number;
-//   sale_master_id?: number;
-//   employee_id: number;
-// }
-
-
 
 
 export class PurchaseTransactionDetail{
@@ -20,7 +9,7 @@ export class PurchaseTransactionDetail{
   purchase_master_id: number;
   sale_master_id?: number;
   employee_id: number;
-  transaction_details: {
+  credit_transaction_details: {
     id?: number;
     transaction_master_id: number;
     transaction_type_id: number;
@@ -33,7 +22,7 @@ export class PurchaseTransactionDetail{
       ledger_group: {id: number; group_name: string; }
     };
     transaction_type: {id: number; transaction_name: string; formal_name: string; transaction_type_value: number};
-  };
+  }[];
   purchase_master: {
     id: number;
     discount: number;
@@ -41,6 +30,6 @@ export class PurchaseTransactionDetail{
     loading_n_unloading_expenditure: number;
     comment: string;
     purchase_details: {id: number; purchase_master_id: number; product_id: number; unit_id: number;
-                  quantity: number; price: number; discount: number};
+                  quantity: number; price: number; discount: number}[];
   };
 }
