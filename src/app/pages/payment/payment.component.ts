15a0165f428fc622ee90315a4f62a6e34b577cbe
Nanda Gopal Sutradhar <ngsutradhar50@gmail.com>
2020-09-19 13:55:41 +0530

    working with payment save

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -4,6 +4,10 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 import {formatDate} from '@angular/common';
 import {VendorService} from '../../services/vendor.service';
 import {Vendor} from '../../models/vendor.model';
+import {PaymentService} from '../../services/payment.service';
+import Swal from 'sweetalert2';
+import {TransactionMaster} from '../../models/transactionMaster.model';
+import {TransactionDetail} from '../../models/transactionDetail.model';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -13,10 +17,10 @@ export class PaymentComponent implements OnInit {
   transactionMasterForm: FormGroup;
   transactionDetailForm: FormGroup;
   vendorList: Vendor[] = [];
-
-
-
-  constructor(private vendorService: VendorService) { }
+  transactionMaster: TransactionMaster;
+  transactionDetails: TransactionDetail[] = [];
+  paymentContainer: {tm: TransactionMaster, td: TransactionDetail[]};
+  constructor(private vendorService: VendorService, private paymentService: PaymentService){ }
 
   ngOnInit(): void {
     this.vendorList = this.vendorService.getVendorList();
@@ -29,5 +33,57 @@ export class PaymentComponent implements OnInit {
     let val = this.transactionMasterForm.value.transaction_date;
     val = formatDate(val, 'yyyy-MM-dd', 'en');
     this.transactionMasterForm.patchValue({transaction_date: val});
+  }
+
+  clearForm() {
+
+  }
+
+  savePayment() {
+    // purchase will be saved from here
+    Swal.fire({
+      title: 'Confirmation',
+      text: 'Do you sure to Save',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Save It!'
+    }).then((result) => {
+      // if selected yes
+      if (result.value) {
+        // will be saved from here
+        // tslint:disable-next-line:max-line-length
+        this.paymentService.savePayment(this.transactionMaster, this.transactionDetails).subscribe(response => {
+          if (response.success === 1){
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Purchase saved',
+              showConfirmButton: false,
+              timer: 3000
+            });
+            this.storage.clear().subscribe(() => {});
+            this.purchaseDetails = [];
+            this.totalPurchaseAmount = 0;
+            this.purchaseMasterForm.reset(this.defaultValues.purchaseMasterForm);
+            this.purchaseDetailForm.reset(this.defaultValues.purchaseDetailsForm);
+            this.transactionMasterForm.reset(this.defaultValues.transactionMasterForm);
+            this.transactionDetailForm.reset(this.defaultValues.transactionDetailsForm);
+            this.currentTab = 2;
+          }
+        }, (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error.message,
+            footer: '<a href>Why do I have this issue?</a>',
+            timer: 0
+          });
+        });
+      }else{
+        // will not be saved
+      }
+    });
   }
 }
--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {formatDate} from '@angular/common';
+import {PurchaseMaster} from '../models/purchaseMaster.model';
+import {PurchaseDetail} from '../models/purchaseDetail.model';
+import {TransactionMaster} from '../models/transactionMaster.model';
+import {TransactionDetail} from '../models/transactionDetail.model';
+import {PurchaseVoucher} from '../models/purchaseVoucher.model';
+import {catchError, tap} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +18,7 @@ export class PaymentService {
   transactionMasterForm: FormGroup;
   transactionDetailForm: FormGroup;
   public userData: {id: number, personName: string, _authKey: string, personTypeId: number};
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.userData = JSON.parse(localStorage.getItem('user'));
     if (!this.userData){
       return;
@@ -34,5 +42,35 @@ export class PaymentService {
       amount: new FormControl(0)
     });
   } // end of constructor
+  private handleError(errorResponse: HttpErrorResponse){
+    // when your api server is not working
+    if (errorResponse.status === 0){
+      alert('your API is not working');
+    }
+    if (errorResponse.status === 401){
+      alert(errorResponse.error.message);
+      // this.router.navigate(['/auth']).then();
+      this.router.navigate(['/owner']).then(r => {console.log(r); });
+      location.reload();
+    }
+
+    if (errorResponse.error.message.includes('1062')){
+      return throwError({success: 0, status: 'failed', message: 'Record already exists', statusText: ''});
+    }else if (errorResponse.error.message.includes('1451')){
+      return throwError({success: 0, status: 'failed', message: 'This record can not be deleted', statusText: ''});
+    }else {
+      return throwError(errorResponse.error.message);
+    }
+  }
+  savePayment(transactionMaster: TransactionMaster, transactionDetails: TransactionDetail[]) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.post<{ success: number, data: PurchaseVoucher }>('http://127.0.0.1:8000/api/payment',
+      {
+        transaction_master: transactionMaster,
+        transaction_details: transactionDetails
+      })
+      .pipe(catchError(this.handleError), tap((response: {success: number, data: PaymentVoucher}) => {
+      }));
+  }
 
 }
